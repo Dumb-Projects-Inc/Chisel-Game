ba11d8cef847e66b888fa69690b5d55e19752cb7
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-04-21 16:22:03 +0200

    remove testing code from main

--- src/main/scala/game/main.scala
+++ src/main/scala/game/main.scala
@@ -12,46 +12,6 @@ class Vec2(precision: Int = 10) extends Bundle {
   val y = UInt(precision.W)
 }
 
-class FixedPointDecimal(val totalBits: Int, val fracBits: Int) extends Bundle {
-  val raw = SInt(totalBits.W)
-
-  def +(that: FixedPointDecimal): FixedPointDecimal = {
-    require(totalBits == that.totalBits && fracBits == that.fracBits)
-    val sum = Wire(new FixedPointDecimal(totalBits, fracBits))
-    sum.raw := this.raw + that.raw
-    sum
-  }
-
-  def -(that: FixedPointDecimal): FixedPointDecimal = {
-    require(totalBits == that.totalBits && fracBits == that.fracBits)
-    val diff = Wire(new FixedPointDecimal(totalBits, fracBits))
-    diff.raw := this.raw - that.raw
-    diff
-  }
-
-  def *(that: FixedPointDecimal): FixedPointDecimal = {
-    require(totalBits == that.totalBits && fracBits == that.fracBits)
-    val prod = Wire(new FixedPointDecimal(totalBits, fracBits))
-    val extended = (this.raw.asSInt * that.raw.asSInt).asSInt
-    prod.raw := (extended >> fracBits).asSInt
-    prod
-  }
-}
-
-class UartEngine(clkSpeed: Int) extends Module {
-  val io = IO(new Bundle {
-    val tx = Output(Bool())
-    val x = Input(new FixedPointDecimal(4, 2))
-    val y = Input(new FixedPointDecimal(4, 2))
-  })
-
-  val uart = Module(new BufferedTx(clkSpeed, 115200))
-  io.tx := uart.io.txd
-
-  val prod = io.x * io.y
-
-}
-
 object gameEngineMain {
   def main(args: Array[String]): Unit = {
     // Splitting files is expected, and the blackbox will generate syntax errors if not split
