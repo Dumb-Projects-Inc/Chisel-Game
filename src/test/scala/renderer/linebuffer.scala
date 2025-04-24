1f7da3fca5e2096444742814b13aa9eef0e73630
Tobias Møller Jensen <29657183+tobias1012@users.noreply.github.com>
2025-04-24 11:24:38 +0200

    update linebuffer with debug and testing that works

--- src/test/scala/renderer/linebuffer.scala
+++ src/test/scala/renderer/linebuffer.scala
@@ -8,9 +8,9 @@ class LinebufferSpec extends AnyFlatSpec {
 
   behavior of "lineBuffer"
   it should "correctly switch on rising edge" in {
-    simulate(new LineBuffer(10)) { dut => 
+    simulate(new LineBuffer(10, debug = true)) { dut => 
       // Initial state
-      dut.bufferSel.expect(false.B)
+      dut.io.bufferSel.get.expect(false.B)
       dut.io.data.expect(0.U)
 
       // Write to buffer
@@ -22,9 +22,10 @@ class LinebufferSpec extends AnyFlatSpec {
       // Switch to buffer 1
       dut.io.switch.poke(true.B)
       dut.clock.step()
-      dut.bufferSel.expect(true.B)
+      dut.io.bufferSel.get.expect(true.B)
       dut.clock.step()
-      dut.bufferSel.expect(true.B) // Ensure buffer 1 is selected 2 cycles later
+      dut.io.bufferSel.get.expect(true.B) // Ensure buffer 1 is selected 2 cycles later
+      dut.io.switch.poke(false.B)
 
       // Write to buffer
       dut.io.wrAddr.poke(1.U)
