7a4003f9451793ada4a2e3f09c321faa75646764
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-05-08 22:40:33 +0200

    Continue work on golden model

--- src/test/scala/raycaster.scala
+++ src/test/scala/raycaster.scala
@@ -6,320 +6,147 @@ import chisel3.simulator.EphemeralSimulator._
 import gameEngine.fixed.FixedPointUtils._
 import org.scalatest.matchers.should.Matchers
 
-class Vec2D(val x: Double, val y: Double) {
-  def norm(): Double = {
-    (x*x + y*y)
+class Vec2D(var x: Double, var y: Double) {
+  override def toString: String = s"(x=$x, y=$y)"
+}
+
+object Vec2D {
+  def apply(x: Double, y: Double): Vec2D = {
+    new Vec2D(x, y)
   }
 }
 
 class RaycasterSpec extends AnyFlatSpec with Matchers {
 
   def expectedDdaPos(
-      startX: Double,
-      startY: Double,
+      start: Vec2D,
       angle: Double,
       nSteps: Int = 0
-  ): (Double, Double) = {
+  ): Vec2D = {
     val pointsEast = angle < math.Pi / 2 || angle > 3 * math.Pi / 2
     val pointsNorth = angle < math.Pi
 
-    val vertical = math.sin(angle) == 0
-    val horizontal = math.cos(angle) == 0
+    val vertical = math.cos(angle) == 0
+    val horizontal = math.sin(angle) == 0
 
     val tan = math.tan(angle)
     val cot = 1.0 / tan
 
     val hRay0 = {
-      val y0 = if (pointsNorth) math.ceil(startY) else math.floor(startY)
-      if (vertical) {
-        new Vec2D(startX, y0)
-      } else if (horizontal) {
-        new Vec2D(Double.PositiveInfinity, y0)
+      val y0 = if (pointsNorth) math.ceil(start.y) else math.floor(start.y)
+      if (horizontal) {
+        Vec2D(Double.PositiveInfinity, start.y)
       } else {
-        new Vec2D(startX + (y0 - startY)*cot, y0)
+        Vec2D(start.x + (y0 - start.y) * cot, y0)
       }
     }
 
     val vRay0 = {
-      val y0 = if (pointsNorth) math.ceil(startY) else math.floor(startY)
+      val x0 = if (pointsEast) math.ceil(start.x) else math.floor(start.x)
       if (vertical) {
-        new Vec2D(Double.PositiveInfinity, y0)
-      } else if (horizontal) {
-        new Vec2D(Double.PositiveInfinity, y0)
+        Vec2D(start.x, Double.PositiveInfinity)
       } else {
-        new Vec2D(startX + (y0 - startY)*cot, y0)
+        Vec2D(x0, start.y + (x0 - start.x) * tan)
       }
     }
-    val hDx = if (pointsNorth) cot else -cot
+
+    val hDx = {
+      if (vertical) {
+        0.0
+      } else if (horizontal) {
+        Double.PositiveInfinity
+      } else {
+        if (pointsNorth) cot else -cot
+      }
+    }
     val hDy = if (pointsNorth) 1.0 else -1.0
+
     val vDx = if (pointsEast) 1.0 else -1.0
-    val vDy = if (pointsEast) tan else -tan
-
-    var hRayX = hRayX0
-    var hRayY = hRayY0
-    var vRayX = vRayX0
-    var vRayY = vRayY0
-
-    def pick() = {
-      val dH2 =
-        (hRayX - startX) * (hRayX - startX) + (hRayY - startY) * (hRayY - startY)
-      val dV2 =
-        (vRayX - startX) * (vRayX - startX) + (vRayY - startY) * (vRayY - startY)
-      if (dH2 <= dV2) (hRayX, hRayY) else (vRayX, vRayY)
+    val vDy = {
+      if (vertical) {
+        Double.PositiveInfinity
+      } else if (horizontal) {
+        0.0
+      } else {
+        if (pointsEast) tan else -tan
+      }
     }
 
-    var (posX, posY) = pick()
+    var hRay = hRay0
+    var vRay = vRay0
+
+    def hRayLen() = {
+      math.sqrt(math.pow(hRay.x - start.x, 2) + math.pow(hRay.y - start.y, 2))
+    }
+
+    def vRayLen()= {
+      math.sqrt(math.pow(vRay.x - start.x, 2) + math.pow(vRay.y - start.y, 2))
+    }
+
+
+    var pos = if (hRayLen() <= vRayLen()) hRay else vRay
 
     for (_ <- 0 to nSteps) {
-      val dH2 =
-        (hRayX - startX) * (hRayX - startX) + (hRayY - startY) * (hRayY - startY)
-      val dV2 =
-        (vRayX - startX) * (vRayX - startX) + (vRayY - startY) * (vRayY - startY)
-      if (dH2 <= dV2) {
-        hRayX += hDx
-        hRayY += hDy
+      if (hRayLen() <= vRayLen()) {
+        hRay.x += hDx
+        hRay.y += hDy
       } else {
-        vRayX += vDx
-        vRayY += vDy
+        vRay.x += vDx
+        vRay.y += vDy
       }
-      val (x, y) = pick()
-      posX = x; posY = y
+
+      if (hRayLen() <= vRayLen()) {
+        pos = hRay
+      } else {
+        pos = vRay
+      }
     }
 
-    (posX, posY)
+    (pos)
   }
 
   behavior of "expectedDdaPos"
   it should "implement golden model correctly" in {
-    val origoTests: Seq[((Double, Double), Double, Int, (Double, Double))] = Seq(
-      // Shoot ray horizontally
-      ((0.0, 0.0),0, 0, (0.0, 0.0)),
-      ((0.0, 0.0),0, 1, (1.0, 0.0)),
-      ((0.0, 0.0),0, 2, (2.0, 0.0)),
-      ((0.0, 0.0),0, 3, (3.0, 0.0)),
+    val origoTests: Seq[(Vec2D, Double, Int, Vec2D)] =
+      Seq(
+        // Shoot ray horizontally
+        (Vec2D(0.0, 0.0), 0, 0, Vec2D(0.0, 0.0)),
+        (Vec2D(0.0, 0.0), 0, 1, Vec2D(1.0, 0.0)),
+        (Vec2D(0.0, 0.0), 0, 2, Vec2D(2.0, 0.0)),
+        (Vec2D(0.0, 0.0), 0, 3, Vec2D(3.0, 0.0)),
 
-      // Shoot ray at 22.5 deg
-      ((0.0, 0.0), math.Pi / 8, 0, (0.0, 0.0)),
-      ((0.0, 0.0), math.Pi / 8, 1, (1.0, 0.4142)),
-      ((0.0, 0.0), math.Pi / 8, 2, (2.0, 0.82)),
-      ((0.0, 0.0), math.Pi / 8, 3, (3.0, 1.2)),
+        // Shoot ray at 22.5 deg
+        (Vec2D(0.0, 0.0), math.Pi / 8, 0, Vec2D(0.0, 0.0)),
+        (Vec2D(0.0, 0.0), math.Pi / 8, 1, Vec2D(1.0, 0.4142)),
+        (Vec2D(0.0, 0.0), math.Pi / 8, 2, Vec2D(2.0, 0.82)),
+        (Vec2D(0.0, 0.0), math.Pi / 8, 3, Vec2D(2.4142, 1.2)),
 
-      // Shoot ray at 45 deg
-      ((0.0, 0.0), math.Pi / 4, 0, (0.0, 0.0)),
-      ((0.0, 0.0), math.Pi / 4, 1, (1.0, 1.0)),
-      ((0.0, 0.0), math.Pi / 4, 2, (2.0, 2.0)),
-      ((0.0, 0.0), math.Pi / 4, 3, (3.0, 3.0)),
-      
+        // Shoot ray at 45 deg
+        (Vec2D(0.0, 0.0), math.Pi / 4, 0, Vec2D(0.0, 0.0)),
+        (Vec2D(0.0, 0.0), math.Pi / 4, 1, Vec2D(1.0, 1.0)),
+        (Vec2D(0.0, 0.0), math.Pi / 4, 2, Vec2D(2.0, 2.0)),
+        (Vec2D(0.0, 0.0), math.Pi / 4, 3, Vec2D(3.0, 3.0)),
 
-      // Shoot ray vertically
-      ((0.0, 0.0), math.Pi / 2, 0, (0.0, 0.0)),
-      ((0.0, 0.0), math.Pi / 2, 1, (0.0, 1.0)),
-      ((0.0, 0.0), math.Pi / 2, 2, (0.0, 2.0)),
-      ((0.0, 0.0), math.Pi / 2, 3, (0.0, 3.0)),
-    )
+        // Shoot ray vertically
+        (Vec2D(0.0, 0.0), math.Pi / 2, 0, Vec2D(0.0, 0.0)),
+        (Vec2D(0.0, 0.0), math.Pi / 2, 1, Vec2D(0.0, 1.0)),
+        (Vec2D(0.0, 0.0), math.Pi / 2, 2, Vec2D(0.0, 2.0)),
+        (Vec2D(0.0, 0.0), math.Pi / 2, 3, Vec2D(0.0, 3.0))
+      )
 
     val tests = origoTests.distinct
-
-
-    for (((x, y), angle, steps, (expX, expY)) <- tests) {
-      val (gotX, gotY) = expectedDdaPos(x, y, angle, steps)
+    for ((start, angle, steps, end) <- tests) {
+      val result = expectedDdaPos(start, angle, steps)
       withClue(
-        f"startPos: (${x},${y}) - angle: ${angle}, - steps: ${steps} |"
+        f"start: ${start} - angle: ${angle}, - steps: ${steps} - coord: X ="
       ) {
-        gotX should be(expX)
-        gotY should be(expY)
+        result.x should be (end.x)
+      }
+      withClue(
+        f"start: ${start} - angle: ${angle}, - steps: ${steps} - coord: X ="
+      ) {
+        result.y should be (end.y)
       }
     }
   }
-
-  behavior of "Raycaster"
-
-  it should "calculate correct intersection for general angles" in {
-    val positions = Seq(
-      (0.01, 0.01),
-      (0.5, 0.5),
-      (0.0, 0.5),
-      (0.5, 0.0),
-      (1.0, 1.0),
-      (0.0, 1.0),
-      (1.0, 0.0)
-    )
-
-    val angles = Seq(
-      0.05,
-      math.Pi / 8,
-      math.Pi / 4,
-      math.Pi / 3,
-      math.Pi / 2 + 0.05,
-      3 * math.Pi / 4,
-      math.Pi,
-      5 * math.Pi / 4,
-      3 * math.Pi / 2,
-      7 * math.Pi / 4
-    )
-    val stepLimits = Seq(0, 1, 4, 8)
-
-    for (maxStep <- stepLimits) {
-
-      simulate(new Raycaster(maxStep)) { dut =>
-        for (pos <- positions; angle <- angles) {
-          val (x, y) = pos
-          dut.io.valid.poke(false.B)
-          dut.io.mapTile.poke(false.B)
-          dut.io.rayStart.x.poke(toFP(x))
-          dut.io.rayStart.y.poke(toFP(y))
-          dut.io.rayAngle.poke(toFP(angle))
-          dut.clock.step()
-
-          dut.io.ready.expect(true.B)
-
-          dut.io.valid.poke(true.B)
-          dut.clock.step()
-          dut.io.valid.poke(false.B)
-          dut.io.ready.expect(false.B)
-
-          var counter = 0
-          dut.clock.stepUntil(dut.io.ready, 1, (maxStep + 1) * 10)
-
-          val (expX, expY) = expectedDdaPos(x, y, angle, maxStep)
-          val gotX = dut.io.pos.x.peek().toDouble
-          val gotY = dut.io.pos.y.peek().toDouble
-
-          val tol = 0.075
-          val errX = math.abs(gotX - expX)
-
-          val errY = math.abs(gotY - expY)
-          if (errX >= tol) {
-            info(
-              f"WARNING: [$x,$y $angle,$maxStep] X off by $errX%.3f " +
-                f"(expected $expX%.3f, got $gotX%.3f)"
-            )
-          }
-          if (errY >= tol) {
-            info(
-              f"WARNING: [${x},${y} $angle,$maxStep] Y off by $errY%.3f (exp=$expY%.3f got=$gotY%.3f)"
-            )
-          }
-
-        }
-      }
-    }
-
-  }
-
-  it should "assert ready in idle, drop ready after init, then reassert upon a complete calculation" in {
-    simulate(new Raycaster(maxSteps = 8)) { dut =>
-      // init
-      dut.io.valid.poke(false.B)
-      dut.io.mapTile.poke(false.B)
-      dut.io.rayStart.x.poke(toFP(0.5))
-      dut.io.rayStart.y.poke(toFP(0.5))
-      dut.io.rayAngle.poke(toFP(math.Pi / 3))
-
-      dut.clock.step()
-      dut.io.ready.expect(
-        true.B,
-        "After reset and setup, ready should be high (idle and waiting for valid)"
-      )
-
-      // Load
-      dut.io.valid.poke(true)
-      dut.clock.step()
-      dut.io.ready.expect(
-        false.B,
-        "Once we assert valid, ready must drop as the ray-cast begins. Should be in sInit"
-      )
-
-      // Force wall hit
-      dut.io.mapTile.poke(true)
-      dut.clock.step()
-      dut.io.ready.expect(false.B, "Still processing, should be in sLoad")
-      dut.clock.step()
-      dut.io.ready.expect(false.B, "Still processing, should be in sCheck")
-      dut.clock.step()
-      dut.io.ready.expect(
-        true.B,
-        "sCheck found that wall was hit and sent us back to idle"
-      )
-
-      val (expX, expY) = expectedDdaPos(0.5, 0.5, math.Pi / 3)
-      val gotX = dut.io.pos.x.peek().toDouble
-      val gotY = dut.io.pos.y.peek().toDouble
-      assert(
-        math.abs(gotX - expX) < 1e-2,
-        f"For x of initial intersection expected ${expX}, got ${gotX}"
-      )
-      assert(
-        math.abs(gotY - expY) < 1e-2,
-        f"For y of initial intersection expected ${expY}, got ${gotY}"
-      )
-
-    }
-  }
-  it should "calculate correct intersection for edgecases" in {
-    val positions = Seq(
-      (0.0, 0.0),
-      (0.0, 0.5),
-      (0.5, 0.0),
-      (0.0, 1.0),
-      (1.0, 0.0),
-      (1.0, 1.0)
-    )
-
-    val angles = Seq(
-      0.0,
-      math.Pi / 2,
-      3 * math.Pi / 4,
-      math.Pi
-    )
-    val stepLimits = Seq(0, 1, 4, 8)
-
-    for (maxStep <- stepLimits) {
-
-      simulate(new Raycaster(maxStep)) { dut =>
-        for (pos <- positions; angle <- angles) {
-          val (x, y) = pos
-          dut.io.valid.poke(false.B)
-          dut.io.mapTile.poke(false.B)
-          dut.io.rayStart.x.poke(toFP(x))
-          dut.io.rayStart.y.poke(toFP(y))
-          dut.io.rayAngle.poke(toFP(angle))
-          dut.clock.step()
-
-          dut.io.ready.expect(true.B)
-
-          dut.io.valid.poke(true.B)
-          dut.clock.step()
-          dut.io.valid.poke(false.B)
-          dut.io.ready.expect(false.B)
-
-          var counter = 0
-          dut.clock.stepUntil(dut.io.ready, 1, (maxStep + 1) * 10)
-
-          val (expX, expY) = expectedDdaPos(x, y, angle, maxStep)
-          val gotX = dut.io.pos.x.peek().toDouble
-          val gotY = dut.io.pos.y.peek().toDouble
-
-          val tol = 0.075
-          val errX = math.abs(gotX - expX)
-
-          val errY = math.abs(gotY - expY)
-          if (errX >= tol) {
-            info(
-              f"WARNING: [$x,$y $angle,$maxStep] X off by $errX%.3f " +
-                f"(expected $expX%.3f, got $gotX%.3f)"
-            )
-          }
-          if (errY >= tol) {
-            info(
-              f"WARNING: [${x},${y} $angle,$maxStep] Y off by $errY%.3f (exp=$expY%.3f got=$gotY%.3f)"
-            )
-          }
-
-        }
-      }
-    }
-
-  }
-
 }
