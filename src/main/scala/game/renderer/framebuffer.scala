83e51dcf82e75c05b75f43063cee85c7045855f5
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-06-08 17:50:46 +0200

    implemented palleted double frame buffer, includes demo

--- src/main/scala/game/renderer/framebuffer.scala
+++ src/main/scala/game/renderer/framebuffer.scala
@@ -1,8 +1,90 @@
 package gameEngine.framebuffer
 
+import gameEngine.screen.VGAInterface
+
 import chisel3._
-import chisel3.util._
 import chisel3.util.experimental.loadMemoryFromFileInline
+import chisel3.util.{log2Ceil, Counter, switch, is}
+import chisel3.stage.ChiselGeneratorAnnotation
+import circt.stage.{ChiselStage, FirtoolOption}
+
+import gameEngine.screen.VGATiming
+
+class DualPaletteFrameBuffer(
+    palette: Seq[UInt]
+) extends Module {
+  private val numColors = palette.length
+  private val width = 320
+  private val height = 240
+  val pal = VecInit(palette)
+
+  val io = IO(new Bundle {
+    val valid = Input(Bool())
+    val wEnable = Input(Bool())
+    val x = Input(UInt(log2Ceil(width).W))
+    val y = Input(UInt(log2Ceil(height).W))
+    val dataIn = Input(UInt(log2Ceil(numColors).W))
+    val vga = Output(new VGAInterface)
+    val newFrame = Output(Bool())
+  })
+
+  val bufferSel = RegInit(false.B)
+
+  val buffer0 = Module(new Buffer(width, height, log2Ceil(numColors)))
+  buffer0.io.enable := true.B
+  buffer0.io.write := false.B
+  buffer0.io.dataIn := 0.U
+  buffer0.io.x := 0.U
+  buffer0.io.y := 0.U
+
+  val buffer1 = Module(new Buffer(width, height, log2Ceil(numColors)))
+  buffer1.io.enable := true.B
+  buffer1.io.write := false.B
+  buffer1.io.dataIn := 0.U
+  buffer1.io.x := 0.U
+  buffer1.io.y := 0.U
+
+  val vga = Module(new VGATiming)
+  io.vga.hsync := vga.io.hSync
+  io.vga.vsync := vga.io.vSync
+
+  val pixelIdx = WireDefault(0.U(log2Ceil(numColors).W))
+  val color = pal(pixelIdx)
+  io.vga.red := Mux(vga.io.visible, color(11, 8), 0.U(4.W))
+  io.vga.green := Mux(vga.io.visible, color(7, 4), 0.U(4.W))
+  io.vga.blue := Mux(vga.io.visible, color(3, 0), 0.U(4.W))
+
+  val newFrame = vga.io.vSync & !RegNext(vga.io.vSync)
+  when(newFrame & io.valid) {
+    bufferSel := ~bufferSel
+  }
+
+  io.newFrame := newFrame
+
+  when(bufferSel) {
+    buffer0.io.x := vga.io.pixelX / 2.U
+    buffer0.io.y := vga.io.pixelY / 2.U
+    pixelIdx := buffer0.io.dataOut
+  }.otherwise {
+    buffer1.io.x := vga.io.pixelX / 2.U
+    buffer1.io.y := vga.io.pixelY / 2.U
+    pixelIdx := buffer1.io.dataOut
+  }
+
+  when(io.wEnable) {
+    when(bufferSel) {
+      buffer1.io.write := true.B
+      buffer1.io.x := io.x
+      buffer1.io.y := io.y
+      buffer1.io.dataIn := io.dataIn
+    }.otherwise {
+      buffer0.io.write := true.B
+      buffer0.io.x := io.x
+      buffer0.io.y := io.y
+      buffer0.io.dataIn := io.dataIn
+    }
+  }
+}
 
 class Buffer(
     width: Int,
@@ -46,4 +128,80 @@ class Buffer(
     io.dataOut := m.io.dout
   }
 
+}
+
+private class TopDemo extends Module {
+  val io = IO(new Bundle {
+    val vga = Output(new VGAInterface)
+    val valid = Input(Bool())
+  })
+
+  val palette = Seq(
+    "hF00".U(12.W), // red
+    "h0F0".U(12.W), // green
+    "h00F".U(12.W), // blue
+    "hFFF".U(12.W) // white
+  )
+
+  val fb = Module(new DualPaletteFrameBuffer(palette))
+  io.vga := fb.io.vga
+
+  object S extends ChiselEnum {
+    val filling, waiting = Value
+  }
+
+  val state = RegInit(S.filling)
+
+  val (x, xWrap) = Counter(state === S.filling, 320)
+  val (y, yWrap) = Counter(xWrap, 240)
+
+  val (count, wrap) = Counter(true.B, 100000000)
+  val (nextColor, nextColorWrap) =
+    Counter(wrap & (state === S.waiting), palette.length)
+
+  fb.io.valid := io.valid
+  fb.io.wEnable := false.B
+  fb.io.x := 0.U
+  fb.io.y := 0.U
+  fb.io.dataIn := 0.U
+
+  switch(state) {
+    is(S.filling) {
+      fb.io.wEnable := true.B
+      fb.io.x := x
+      fb.io.y := y
+      fb.io.dataIn := nextColor
+
+      when(xWrap & yWrap) {
+        state := S.waiting
+      }
+
+    }
+
+    is(S.waiting) {
+      when(fb.io.newFrame) {
+        state := S.filling
+      }
+    }
+  }
+
+}
+
+object bufferMain {
+  def main(args: Array[String]): Unit = {
+    (new ChiselStage).execute(
+      Array(
+        "--target",
+        "systemverilog",
+        "--target-dir",
+        "generated",
+        "--split-verilog"
+      ),
+      Seq(
+        ChiselGeneratorAnnotation(() => new TopDemo),
+        FirtoolOption("--disable-all-randomization")
+      )
+    )
+
+  }
 }
