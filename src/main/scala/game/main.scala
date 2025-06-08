46c04d866c1f8c8d4242877266b998b0410fbd38
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-06-08 23:04:26 +0200

    add plan for future

--- src/main/scala/game/main.scala
+++ src/main/scala/game/main.scala
@@ -6,55 +6,66 @@ import chisel3.util.PriorityMux
 import chisel3.stage.ChiselGeneratorAnnotation
 import circt.stage.{ChiselStage, FirtoolOption}
 
-import gameEngine.screen._
-import gameEngine.renderer.RainbowRenderer
-import gameEngine.entity.SpriteEntity
-import gameEngine.entity.library.SmileyEntity
-import gameEngine.entity.library.WallEntity
-import gameEngine.vec2.Vec2
-import gameEngine.framebuffer.Buffer
-import chisel3.util.MuxCase
+import gameEngine.screen.VGAInterface
+import gameEngine.framebuffer.DualPaletteFrameBuffer
+
+// TODO:
+// Generer background igennem tilemap
+// - Gem en af hvert tile
+// - gem 2d array som repræsenterer verdenen. Hver index holder en ref til et tile
+// - skal være scrollable
+//
+//
+// Sprite/Entity interface
+// - Entities repræsenterer en ting i spillet. Indeholder:
+//    - interface som gør den renderbar for scene
+//    - interface så spil logik kan interagere med den
+//
+//
+class Scene2D(numColors: Int) extends Module {
+  val io = IO(new Bundle {
+    // Control signal
+    val done = Output(Bool())
+    val next = Input(Bool())
+
+    // Signals used to write to buffer
+    val x = Output(UInt(log2Ceil(320).W))
+    val y = Output(UInt(log2Ceil(240).W))
+    val write = Output(Bool())
+    val paletteOut = Output(UInt(log2Ceil(numColors).W))
+  })
+
+}
 
 class Engine extends Module {
   val io = IO(new Bundle {
     val vga = new VGAInterface
   })
 
-  val controller = Module(new VGAController)
-  io.vga := controller.io.vga
+  val doomPalette = Seq(
+    "h000".U(12.W), // #000000
+    "h222".U(12.W), // #222222
+    "h444".U(12.W), // #444444
+    "h777".U(12.W), // #777777
+    "hAAA".U(12.W), // #AAAAAA
+    "hFFF".U(12.W), // #FFFFFF
 
-  val mem = Module(new Buffer(320, 240, 4, Some("framebuffer_init.mem")))
+    "h430".U(12.W), // #443300
+    "h640".U(12.W), // #664400
+    "hA50".U(12.W), // #AA5500
+    "hD90".U(12.W), // #DD9900
 
-  mem.io.write := false.B
-  mem.io.enable := true.B
-  mem.io.dataIn := DontCare
-  mem.io.x := controller.io.x / 2.U
-  mem.io.y := controller.io.y / 2.U
+    "hF20".U(12.W), // #FF2200
+    "hF60".U(12.W), // #FF6600
+    "hFF0".U(12.W), // #FFFF00
 
-  val colorPalette = VecInit(
-    Seq(
-      "h000".U(12.W), //  0: #000000 Black
-      "h00A".U(12.W), //  1: #0000AA Blue
-      "h0A0".U(12.W), //  2: #00AA00 Green
-      "h0AA".U(12.W), //  3: #00AAAA Cyan
-      "hA00".U(12.W), //  4: #AA0000 Red
-      "hA0A".U(12.W), //  5: #AA00AA Magenta
-      "hA50".U(12.W), //  6: #AA5500 Brown
-      "hAAA".U(12.W), //  7: #AAAAAA Light Gray
-      "h555".U(12.W), //  8: #555555 Dark Gray
-      "h55F".U(12.W), //  9: #5555FF Light Blue
-      "h5F5".U(12.W), // 10: #55FF55 Light Green
-      "h5FF".U(12.W), // 11: #55FFFF Light Cyan
-      "hF55".U(12.W), // 12: #FF5555 Light Red
-      "hF5F".U(12.W), // 13: #FF55FF Light Magenta
-      "hFF5".U(12.W), // 14: #FFFF55 Yellow
-      "hFFF".U(12.W) // 15: #FFFFFF White
-    )
+    "hA00".U(12.W), // #AA0000
+    "hF00".U(12.W), // #FF0000
+    "h800".U(12.W) // #880000
   )
 
-  val pixelIndex = mem.io.dataOut
-  val pixelColor = colorPalette(pixelIndex)
-  controller.io.pixel := pixelColor
+  val buf = Module(new DualPaletteFrameBuffer(doomPalette))
+  io.vga := buf.io.vga
 
 }
 object gameEngineMain {
