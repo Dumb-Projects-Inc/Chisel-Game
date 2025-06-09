2df4f1b8fcf3cff392888d72c6de4716152ab3a9
Tobias Møller Jensen <29657183+tobias1012@users.noreply.github.com>
2025-06-09 12:39:19 +0200

    added chisel modules for memory, tests better in verilator

--- src/main/scala/game/util/memory.scala
+++ src/main/scala/game/util/memory.scala
@@ -6,11 +6,17 @@ package gameEngine.util
 // Comments:
 // Wrapper for verilog single port write first memory with initialization
 // implemented as Verilog back box.
+// Also implemented chisel modules:
+// Implementation inspired by the wrapper given from Lucca Pezzarossa
+// This implementation intends to behave the same way, but without blackbox
 //////////////////////////////////////////////////////////////////////////////
 
 import chisel3._
 import chisel3.util.HasBlackBoxResource
+import chisel3.util.experimental.loadMemoryFromFile
+import firrtl.annotations.MemoryLoadFileType
 
+// Only works with vivado
 class RamInitSpWf(dWidth: Int, addrWidth: Int, loadFile: String)
     extends BlackBox(
       Map(
@@ -31,6 +37,36 @@ class RamInitSpWf(dWidth: Int, addrWidth: Int, loadFile: String)
   addResource("/RamInitSpWf.v")
 }
 
+class InitSinglePortWriteFirstRAM(dWidth: Int, addrWidth: Int, loadFile: String)
+    extends Module {
+  val io = IO(new Bundle {
+    val we = Input(Bool())
+    val en = Input(Bool())
+    val addr = Input(UInt(addrWidth.W))
+    val di = Input(UInt(dWidth.W))
+    val dout = Output(UInt(dWidth.W))
+  })
+
+  val ram = SyncReadMem(1 << addrWidth, UInt(dWidth.W))
+  val doutReg = RegInit(0.U(dWidth.W))
+  loadMemoryFromFile(ram, loadFile)
+
+  when(io.en) {
+    when(io.we) {
+      ram.write(io.addr, io.di)
+      doutReg := io.di
+    }.otherwise {
+      doutReg := ram.read(
+        io.addr,
+        true.B
+      )
+    }
+  }
+
+  io.dout := doutReg
+}
+
+//Only works with vivado
 class RamSpWf(dWidth: Int, addrWidth: Int)
     extends BlackBox(Map("ADDR_WIDTH" -> addrWidth, "DATA_WIDTH" -> dWidth))
     with HasBlackBoxResource {
@@ -43,4 +79,31 @@ class RamSpWf(dWidth: Int, addrWidth: Int)
     val dout = Output(UInt(dWidth.W))
   })
   addResource("/RamSpWf.v")
+}
+
+class SinglePortWriteFirstRAM(dWidth: Int, addrWidth: Int) extends Module {
+  val io = IO(new Bundle {
+    val we = Input(Bool())
+    val en = Input(Bool())
+    val addr = Input(UInt(addrWidth.W))
+    val di = Input(UInt(dWidth.W))
+    val dout = Output(UInt(dWidth.W))
+  })
+
+  val ram = SyncReadMem(1 << addrWidth, UInt(dWidth.W))
+  val doutReg = RegInit(0.U(dWidth.W))
+
+  when(io.en) {
+    when(io.we) {
+      ram.write(io.addr, io.di)
+      doutReg := io.di
+    }.otherwise {
+      doutReg := ram.read(
+        io.addr,
+        true.B
+      )
+    }
+  }
+
+  io.dout := doutReg
 }
--- src/test/scala/renderer/framebuffer.scala
+++ src/test/scala/renderer/framebuffer.scala
@@ -18,6 +18,7 @@ class BufferSpec extends AnyFunSpec with ChiselSim with Matchers {
         dut.clock.step()
 
         dut.io.write.poke(false.B)
+        // Test will fail with new RAM, however this is because verilator is testing blackbox wrong
         dut.clock.step()
         dut.io.dataOut.expect(2.U)
 
