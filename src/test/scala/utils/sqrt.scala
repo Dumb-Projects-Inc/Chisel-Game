c21bc4ed4142eecb609dd77cc57db9a37eaaa24b
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-06-14 23:25:56 +0200

    inv sqrt is stable for inputs ]0;64]

--- src/test/scala/utils/sqrt.scala
+++ src/test/scala/utils/sqrt.scala
@@ -1,66 +1,57 @@
 package gameEngine.fixed
 
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funspec.AnyFunSpec
 import chisel3._
-import chisel3.simulator.EphemeralSimulator._
+import chisel3.simulator.scalatest.ChiselSim
 import gameEngine.fixed.InverseSqrt
 import gameEngine.fixed.FixedPointUtils._
+import org.scalatest.matchers.should.Matchers
 
-class SqrtSpec extends AnyFlatSpec {
-  behavior of "Sqrt"
+class SqrtSpec extends AnyFunSpec with ChiselSim with Matchers {
+  describe("Sqrt") {
+    it("compute the inverse sqrt") {
+      val cases = Seq(
+        1, 2, 1.5, 0.5, 0.25, 0.125, 20
+        // 0-300
+      )
+      simulate(new InverseSqrt) { dut =>
+        for (input <- cases) {
+          withClue(input) {
+            dut.io.input.ready.expect(true.B)
+            dut.io.input.bits.poke(toFP(input))
+            dut.io.input.valid.poke(true.B)
+            dut.clock.stepUntil(dut.io.result.valid, 1, 100)
 
-  val errormargin = 0.2
-  def checkMargin(actual: Double, expected: Double): Boolean = {
-    val diff = Math.abs(actual - expected)
-    diff <= errormargin
-  }
-
-  it should "compute the inverse sqrt" in {
-    val cases = Seq(
-      1, 2, 1.5, 0.5, 0.25, 0.125, 20
-      // 0-300
-    )
-    simulate(new InverseSqrt) { dut =>
-      for (input <- cases) {
-        dut.clock.step()
-        dut.io.input.poke(toFP(input))
-        dut.io.wrEn.poke(true.B)
-        dut.clock.step()
-        dut.io.wrEn.poke(false.B)
-
-        dut.clock.step(10)
-
-        val output = fromFP(dut.io.output.peek())
-        dut.io.ready.expect(true.B)
-        assert(
-          checkMargin(output, 1.0 / Math.sqrt(input)),
-          s"Failed for input $input: got $output expected ${1.0 / Math.sqrt(input)}"
-        )
+            dut.io.result.valid.expect(true.B)
+            val result = dut.io.result.bits.peek().toDouble
+            result should be(1.0 / math.sqrt(input) +- 0.1)
+            dut.io.result.ready.poke(true.B)
+            dut.clock.step()
+          }
+        }
       }
     }
   }
-  it should "handle random cases" in {
+  it("handle random cases") {
     val random = new scala.util.Random(42) // Fixed seed
     val cases = Seq.fill(200)(
-      random.nextDouble() * 100 + 1
+      random.nextDouble() * 64
     ) // Random inputs between 1 and 300
 
     simulate(new InverseSqrt) { dut =>
       for (input <- cases) {
-        dut.io.input.poke(toFP(input))
-        dut.io.wrEn.poke(true.B)
-        dut.clock.step()
-        dut.io.wrEn.poke(false.B)
+        withClue(input.toString() + "\n") {
+          dut.io.input.ready.expect(true.B)
+          dut.io.input.bits.poke(toFP(input))
+          dut.io.input.valid.poke(true.B)
+          dut.clock.stepUntil(dut.io.result.valid, 1, 100)
 
-        dut.clock.step(10)
-
-        val output = fromFP(dut.io.output.peek())
-        val output = fromFP(dut.io.output.peek())
-        assert(
-          checkMargin(output, 1.0 / Math.sqrt(input)),
-          s"Failed for input $input: got $output, expected ${1.0 / Math.sqrt(input)}"
-        )
-        print("Clocks taken: " + counter + "\n")
+          dut.io.result.valid.expect(true.B)
+          val result = dut.io.result.bits.peek().toDouble
+          result should be(1.0 / math.sqrt(input) +- 0.1)
+          dut.io.result.ready.poke(true.B)
+          dut.clock.step()
+        }
       }
     }
   }
