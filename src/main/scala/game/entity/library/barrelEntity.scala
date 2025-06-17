17d0c59df331ca0dcf51233a343be127921ee2a4
s245234 <s245234@dtu.dk>
2025-06-17 22:12:42 +0200

    Sprites for BOB

---
+++ src/main/scala/game/entity/library/barrelEntity.scala
@@ -0,0 +1,52 @@
+package gameEngine.entity.library
+
+import chisel3._
+import chisel3.util._
+import gameEngine.entity.PalettedSpriteEntity
+
+
+class BarrelEntity(width: Int, palette: Seq[UInt]) extends Module {
+  val io = IO(new Bundle {
+    val screen = new Bundle {
+      val x = Input(UInt(width.W))
+      val y = Input(UInt(width.W))
+    }
+    val setPos = new Bundle {
+      val wrEn = Input(Bool())
+      val x = Input(UInt(width.W))
+      val y = Input(UInt(width.W))
+    }
+    val scale = Input(UInt(12.W))
+    val visible = Output(Bool())
+    val pixel = Output(UInt(12.W))
+  })
+
+  val posX = RegInit(0.U(width.W))
+  val posY = RegInit(0.U(width.W))
+
+  val sprite = Module(new PalettedSpriteEntity("./barrel.png", width, palette, 64, 64))  
+  sprite.scale := io.scale
+
+  when(io.setPos.wrEn) {
+    posX := io.setPos.x
+    posY := io.setPos.y
+  }
+
+  sprite.io.in.screen.x := io.screen.x
+  sprite.io.in.screen.y := io.screen.y
+
+  sprite.io.in.pos.wrEn.get := true.B
+  sprite.io.in.pos.x := posX
+  sprite.io.in.pos.y := posY
+
+  sprite.io.in.speed.wrEn.get := false.B
+  sprite.io.in.acceleration.wrEn.get := false.B
+
+  sprite.io.in.speed.x := DontCare
+  sprite.io.in.speed.y := DontCare
+  sprite.io.in.acceleration.x := DontCare
+  sprite.io.in.acceleration.y := DontCare
+
+  io.visible := sprite.io.visible
+  io.pixel := sprite.io.pixel
+}
